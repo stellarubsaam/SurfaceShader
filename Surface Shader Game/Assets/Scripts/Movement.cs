06135c5b4c656ff4157a24197e72850574860e42
Stella Rübsaam <43574554+stellarubsaam@users.noreply.github.com>
2020-09-16 14:59:27 +0200

    Logboek + Unity project toegevoegd

---
+++ Surface Shader Game/Assets/Scripts/Movement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed;
+    public float turnSpeed;
+    float moveX;
+    float moveY;
+    Animator anim;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        moveX = Input.GetAxis("Horizontal");
+        moveY = Input.GetAxis("Vertical");
+        if (moveY != 0)
+        {
+            anim.SetBool("moving", true);
+        }
+        else
+        {
+            anim.SetBool("moving", false);
+        }
+        transform.position += transform.forward * speed * moveY / 50;
+        transform.Rotate(Vector3.up * moveX * turnSpeed);
+    }
+}
---
+++ Surface Shader Game/Assets/Scripts/PickUp.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    public GameObject leftShoulder;
+    public GameObject rightShoulder;
+    public GameObject leftLeg;
+    public GameObject rightLeg;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Fruit")
+        {
+            Vector3 scale = new Vector3(0.1f, 0, 0.1f);
+            leftLeg.transform.localScale += scale;
+            rightLeg.transform.localScale += scale;
+            leftShoulder.transform.localScale += scale;
+            rightShoulder.transform.localScale += scale;
+            Destroy(other.gameObject);
+        }
+    }
+}
